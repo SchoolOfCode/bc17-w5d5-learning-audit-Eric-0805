ae06613a6858895a157561cf4d7b94cbe1c7b4d3
Eric-0805 <ericroberts805@gmail.com>
2024-08-05 07:51:20 +0100

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeText() {
+    // selects gthe header element by id and changes its text
+    const title = document.getElementById('title')
+    title.textContent = 'Updated Title';
+}
+
+function changeParagraph() {
+    const paragraph = document.getElementById('paragraph');
+    paragraph.textContent = 'Updated Paragaraph';
+}
